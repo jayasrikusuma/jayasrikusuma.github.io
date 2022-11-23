b6a90f71f6511d539ff3c642104fb48727620e3b
jayasrikusuma <116686409+jayasrikusuma@users.noreply.github.com>
2022-11-23 21:34:05 +0000

    Update nintyNineBottlesOfBeer.js

--- WK9 - Rock Paper Scissors - Loops/99-bottles-of-beer/nintyNineBottlesOfBeer.js
+++ WK9 - Rock Paper Scissors - Loops/99-bottles-of-beer/nintyNineBottlesOfBeer.js
@@ -5,7 +5,7 @@ function showLyrics(){
         
         if( i == 0 ){
             document.getElementById("content").innerHTML += "<br\> No more bottles of beer on the wall, no more bottles of bee <br\> Go to the store and buy some more, 99 bottles of beer on the wall <br\>";
-            return; // Adding return here so it doesn't print a line for the zero.
+            return; 
         }else if (i==1){
             
             document.getElementById("content").innerHTML +="<br\>"+ i + " bottle of beer on the wall," + i + " bottle of beer. <br\> Take one down and pass it around,"+ (i-1) + " bottle of beer on the wall<br\>";
