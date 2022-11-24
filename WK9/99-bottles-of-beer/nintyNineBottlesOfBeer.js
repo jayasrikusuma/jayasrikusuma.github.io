7d881d5480707a2d462780a0c8d4e8ee91640613
jayasrikusuma <116686409+jayasrikusuma@users.noreply.github.com>
2022-11-24 22:28:22 +0000

    Add files via upload

---
+++ WK9/99-bottles-of-beer/nintyNineBottlesOfBeer.js
@@ -0,0 +1,19 @@
+//99-bottles-of-beer
+
+function showLyrics(){
+    for (i = 99; i >= 0; i--){
+        
+        if( i == 0 ){
+            document.getElementById("content").innerHTML += "<br\> No more bottles of beer on the wall, no more bottles of bee <br\> Go to the store and buy some more, 99 bottles of beer on the wall <br\>";
+            return; // Adding return here so it doesn't print a line for the zero.
+        }else if (i==1){
+            
+            document.getElementById("content").innerHTML +="<br\>"+ i + " bottle of beer on the wall," + i + " bottle of beer. <br\> Take one down and pass it around,"+ (i-1) + " bottle of beer on the wall<br\>";
+         }
+        else { 
+            
+            document.getElementById("content").innerHTML +="<br\>"+ i + " bottles of beer on the wall, " + i + " bottles of beer. <br\> Take one down and pass it around," + (i-1) + " bottles of beer on the wall<br\>";
+        }
+		
+    }
+}
