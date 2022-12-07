7d71836cf0e5c6c7788d90a5626f1a30183ac9d3
jayasrikusuma <116686409+jayasrikusuma@users.noreply.github.com>
2022-12-07 15:33:54 +0000

    Delete JMZ/v5.1 directory

--- JMZ/v5.1/pages/about/about.js
+++
@@ -1,7 +0,0 @@
-//Author: Jayasri Kusuma
-
-// Default values for Login ID and Cart value
-document.getElementById('cart').innerHTML="0 \(€\)" ;	
-
-//local storage variable used to store a default value on website first loading - index.html
-document.getElementById('LoginId').innerHTML = "John";   
